d4a6de935aa87b65c3f9173bf030e30bd673cb6a
rafiadpr <work.rafiadrianp@gmail.com>
2025-02-24 20:53:58 +0700

    add percobaan 1 2 3 for jobsheet 3

---
+++ MahasiswaDemo23.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class MahasiswaDemo23 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Mahasiswa23[] arrayOfMahasiswa = new Mahasiswa23[3];
+        String dummy;
+
+        for (int i = 0; i < 3; i++) {
+            arrayOfMahasiswa[i] = new Mahasiswa23();
+            System.out.println("Masukkan data mahasiswa ke-" + (i + 1));
+            System.out.print("NIM          : ");
+            arrayOfMahasiswa[i].nim = input.nextLine();
+            System.out.print("Nama         : ");
+            arrayOfMahasiswa[i].nama = input.nextLine();
+            System.out.print("Kelas        : ");
+            arrayOfMahasiswa[i].kelas = input.nextLine();
+            System.out.print("IPK          : ");
+            dummy = input.nextLine();
+            arrayOfMahasiswa[i].ipk = Float.parseFloat(dummy);
+            System.out.println("=========================================");
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Data mahasiswa ke-" + (i + 1));
+            System.out.println("NIM          : " + arrayOfMahasiswa[i].nim);
+            System.out.println("Nama         : " + arrayOfMahasiswa[i].nama);
+            System.out.println("Kelas        : " + arrayOfMahasiswa[i].kelas);
+            System.out.println("IPK          : " + arrayOfMahasiswa[i].ipk);
+            System.out.println("=========================================");
+        }
+    }
+}
