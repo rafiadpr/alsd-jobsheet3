1d84514165795897c27f65d88fc71cecf8632de0
rafiadpr <work.rafiadrianp@gmail.com>
2025-02-24 21:54:19 +0700

    add percobaan 3 no 1

--- MahasiswaDemo23.java
+++ MahasiswaDemo23.java
@@ -23,11 +23,7 @@ public class MahasiswaDemo23 {
 
         for (int i = 0; i < 3; i++) {
             System.out.println("Data mahasiswa ke-" + (i + 1));
-            System.out.println("NIM          : " + arrayOfMahasiswa[i].nim);
-            System.out.println("Nama         : " + arrayOfMahasiswa[i].nama);
-            System.out.println("Kelas        : " + arrayOfMahasiswa[i].kelas);
-            System.out.println("IPK          : " + arrayOfMahasiswa[i].ipk);
-            System.out.println("=========================================");
+            arrayOfMahasiswa[i].cetakInfo();
         }
     }
 }
--- MataKuliah23.java
+++ MataKuliah23.java
@@ -10,4 +10,18 @@ public class MataKuliah23 {
         this.sks = sks;
         this.jumlahJam = jumlahJam;
     }
+
+    public MataKuliah23(String kode, String nama, int sks) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = sks * 2;
+    }
+
+    public MataKuliah23() {
+        this.kode = "XXX";
+        this.nama = "Mata Kuliah Default";
+        this.sks = 0;
+        this.jumlahJam = 0;
+    }
 }
