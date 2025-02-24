328b7e6afe1a90b839469a5c9a1ee6879e21af63
rafiadpr <work.rafiadrianp@gmail.com>
2025-02-24 21:56:38 +0700

    add percobaan 3 no 4

--- MataKuliahDemo23.java
+++ MataKuliahDemo23.java
@@ -3,17 +3,21 @@ import java.util.Scanner;
 public class MataKuliahDemo23 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        MataKuliah23[] arrayOfMataKuliah = new MataKuliah23[3];
+
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int jumlahMatkul = Integer.parseInt(input.nextLine());
+
+        MataKuliah23[] arrayOfMataKuliah = new MataKuliah23[jumlahMatkul];
         // String kode, nama, dummy;
         // int sks, jumlahJam;
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < jumlahMatkul; i++) {
             System.out.println("Masukkan data mata kuliah ke-" + (i + 1));
             arrayOfMataKuliah[i] = new MataKuliah23();
             arrayOfMataKuliah[i].tambahData();
         }
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < jumlahMatkul; i++) {
             System.out.println("Data mata kuliah ke-" + (i + 1));
             arrayOfMataKuliah[i].cetakInfo();
         }
