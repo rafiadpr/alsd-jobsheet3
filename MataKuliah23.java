d0eb819d89d1cd1e9023621cf36ab7bb61308417
rafiadpr <work.rafiadrianp@gmail.com>
2025-02-24 21:54:57 +0700

    add percobaan 3 no 2

--- MataKuliah23.java
+++ MataKuliah23.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class MataKuliah23 {
+
     public String kode;
     public String nama;
     public int sks;
     public int jumlahJam;
+
+    public MataKuliah23() {
+        
+    }
 
     public MataKuliah23(String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
@@ -11,17 +18,29 @@ public class MataKuliah23 {
         this.jumlahJam = jumlahJam;
     }
 
-    public MataKuliah23(String kode, String nama, int sks) {
-        this.kode = kode;
-        this.nama = nama;
-        this.sks = sks;
-        this.jumlahJam = sks * 2;
+    public void tambahData() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Kode         : ");
+        this.kode = input.nextLine();
+        System.out.print("Nama         : ");
+        this.nama = input.nextLine();
+        System.out.print("SKS          : ");
+        this.sks = Integer.parseInt(input.nextLine());
+        System.out.print("Jumlah Jam   : ");
+        this.jumlahJam = Integer.parseInt(input.nextLine());
+        System.out.println("=========================================");
     }
 
-    public MataKuliah23() {
-        this.kode = "XXX";
-        this.nama = "Mata Kuliah Default";
-        this.sks = 0;
-        this.jumlahJam = 0;
-    }
+    // public MataKuliah23(String kode, String nama, int sks) {
+    //     this.kode = kode;
+    //     this.nama = nama;
+    //     this.sks = sks;
+    //     this.jumlahJam = sks * 2;
+    // }
+    // public MataKuliah23() {
+    //     this.kode = "XXX";
+    //     this.nama = "Mata Kuliah Default";
+    //     this.sks = 0;
+    //     this.jumlahJam = 0;
+    // }
 }
--- MataKuliahDemo23.java
+++ MataKuliahDemo23.java
@@ -4,24 +4,13 @@ public class MataKuliahDemo23 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         MataKuliah23[] arrayOfMataKuliah = new MataKuliah23[3];
-        String kode, nama, dummy;
-        int sks, jumlahJam;
+        // String kode, nama, dummy;
+        // int sks, jumlahJam;
 
         for (int i = 0; i < 3; i++) {
             System.out.println("Masukkan data mata kuliah ke-" + (i + 1));
-            System.out.print("Kode         : ");
-            kode = input.nextLine();
-            System.out.print("Nama         : ");
-            nama = input.nextLine();
-            System.out.print("SKS          : ");
-            dummy = input.nextLine();
-            sks = Integer.parseInt(dummy);
-            System.out.print("Jumlah Jam   : ");
-            dummy = input.nextLine();
-            jumlahJam = Integer.parseInt(dummy);
-            System.out.println("=========================================");
-
-            arrayOfMataKuliah[i] = new MataKuliah23(kode, nama, sks, jumlahJam);
+            arrayOfMataKuliah[i] = new MataKuliah23();
+            arrayOfMataKuliah[i].tambahData();
         }
 
         for (int i = 0; i < 3; i++) {
