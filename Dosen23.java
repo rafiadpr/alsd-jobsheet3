6e88cc60fadc4922d6c064b7bc216fec08c9fc25
rafiadpr <work.rafiadrianp@gmail.com>
2025-02-25 12:49:20 +0700

    changes on tugas no 3 : moving penambahan dosen to dosen23 for shorter code

--- Dosen23.java
+++ Dosen23.java
@@ -1,4 +1,8 @@
+
+import java.util.Scanner;
+
 public class Dosen23 {
+
     public String kode;
     public String nama;
     public boolean jenisKelamin;
@@ -17,5 +21,22 @@ public class Dosen23 {
         System.out.println("Jenis Kelamin : " + (jenisKelamin ? "Pria" : "Wanita"));
         System.out.println("Usia          : " + usia);
         System.out.println("=========================================");
+    }
+
+    public static Dosen23 tambahDosen(Scanner input, int nomor) {
+        System.out.println("Masukkan data dosen ke-" + nomor);
+        System.out.print("Kode: ");
+        String kode = input.nextLine();
+        System.out.print("Nama: ");
+        String nama = input.nextLine();
+        System.out.print("Jenis Kelamin (P/W): ");
+        char jeniskel = input.nextLine().charAt(0);
+        boolean jenisKelamin = (jeniskel == 'P' || jeniskel == 'p');
+        System.out.print("Usia: ");
+        int usia = input.nextInt();
+        input.nextLine();
+        System.out.println("=========================================");
+
+        return new Dosen23(kode, nama, jenisKelamin, usia);
     }
 }
--- DosenDemo23.java
+++ DosenDemo23.java
@@ -11,19 +11,7 @@ public class DosenDemo23 {
 
         Dosen23[] arrayOfDosen = new Dosen23[jumlahDosen];
         for (int i = 0; i < jumlahDosen; i++) {
-            System.out.println("Masukkan data dosen ke-" + (i + 1));
-            System.out.print("Kode: ");
-            String kode = input.nextLine();
-            System.out.print("Nama: ");
-            String nama = input.nextLine();
-            System.out.print("Jenis Kelamin (P/W): ");
-            char jeniskel = input.nextLine().charAt(0);
-            boolean jenisKelamin = (jeniskel == 'P' || jeniskel == 'p');
-            System.out.print("Usia: ");
-            int usia = input.nextInt();
-            input.nextLine();
-            System.out.println("=========================================");
-            arrayOfDosen[i] = new Dosen23(kode, nama, jenisKelamin, usia);
+            arrayOfDosen[i] = Dosen23.tambahDosen(input, i + 1);
         }
 
         // for (int i = 0; i < jumlahDosen; i++) {
